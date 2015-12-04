70f130d8f479144e4cf2f6a63c769204262ee849
Harry Grillo <harry@redbassett.com>
2015-12-04 14:59:30 -0500

    Updated method for first part

--- 4/solution1.rb
+++ 4/solution1.rb
@@ -2,7 +2,7 @@ require "digest/md5"
 
 def solve_puzzle input
     i = 0
-    until Digest::MD5.hexdigest('%s%05i' % [input.strip, i])[0..4] == '00000'
+    until Digest::MD5.hexdigest('%s%i' % [input.strip, i])[0..4] == '00000'
         i += 1
     end
     i
